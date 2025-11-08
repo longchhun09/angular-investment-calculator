0aabd3e9f402d0775a1116595d40e3bbf3c5f007
longchhun <mark.chhunlong@gmail.com>
2025-11-08 11:45:27 +0700

    Refactor app module to use UserInputModule for user input component, removing direct declarations and imports. Introduce user input module for better modularity and organization.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { UserInputComponent } from './components/user-input/user-input.component';
 import { InvestmentResultsComponent } from './components/investment-results/investment-results.component';
-import { FormsModule } from '@angular/forms';
-import { CurrencyPipe } from '@angular/common';
+import { UserInputModule } from './components/user-input/user-input.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, UserInputComponent, InvestmentResultsComponent],
-  imports: [BrowserModule, FormsModule, CurrencyPipe],
+  declarations: [AppComponent, HeaderComponent, InvestmentResultsComponent],
+  imports: [BrowserModule, UserInputModule],
   providers: [],
   bootstrap: [AppComponent]
 })
